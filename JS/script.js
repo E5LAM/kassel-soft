447ed2fecde33fa303b2fae704e5a8f858431f20
Eslam-Hussien <eslam01003733553@gmail.com>
2024-03-29 20:21:41 +0200

    contact us form

--- JS/script.js
+++ JS/script.js
@@ -46,23 +46,59 @@ cards.forEach(card => {
       //  CONTACT US (SEND EMAIL) //
 
 const form = document.querySelector("form");
+const fullName = document.getElementById("name")
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const subject = document.getElementById("subject")
+const message = document.getElementById("message")
 
 function sendEmail(){
+  const bodyMessage = `Full Name: ${fullName.value} <br>
+  Email: ${email.value} <br> Phone Number: ${phone.value} <br>
+  Message: ${message.value} `;
+
   Email.send({
     Host : "smtp.elasticemail.com",
     Username : "kasselsoft@kasselsoft.com",
-    Password : "DAE2079B061C00C7C71ED4C65ABF5D0A2467",
+    Password : "BF446770BDF25335CCE4B9CDC25BC9CE79D3",
     To : 'kasselsoft@kasselsoft.com',
     From : "kasselsoft@kasselsoft.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
+    Subject : subject.value,
+    Body : bodyMessage
   }).then(
-    message => alert(message)
+    message => {
+      if(message == "OK"){
+        Swal.fire({
+          title: "Success!",
+          text: "Message sent Successfully",
+          icon: "success"
+        });
+      }
+    }
   );
+}
+
+function checkInputs(){
+  const items = querySelectorAll(".item")
+  for(const item of  items ){
+    if(item.value == ""){
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    item.addEventListener("keyup" , (e) =>{
+      if(item.value != ""){
+      item.classList.remove("error");
+      item.parentElement.classList.remove("error");
+      }else{
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+      }
+    })
+  }
 }
 
 form.addEventListener("submit", (e) =>{
   e.preventDefault();
-
   sendEmail();
 })
